449dd98d98679a175137c3dbcb93483604885a20
Adeniran Opeyemi Taiwo <60190827+opethaiwoh@users.noreply.github.com>
2025-09-07 19:47:12 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,13 +15,13 @@ import { getFirestore } from "firebase/firestore"; // Added back for career data
 
 // Your web app's Firebase configuration for Favored Online project
 const firebaseConfig = {
-  apiKey: "AIzaSyDgVyHTsiJXDkRJ_osP6EAcQSMIHRndSDg",
-  authDomain: "favored-online-f3e8b.firebaseapp.com",
-  projectId: "favored-online-f3e8b",
-  storageBucket: "favored-online-f3e8b.firebasestorage.app",
-  messagingSenderId: "786977722322",
-  appId: "1:786977722322:web:7d2dad47e024f17b2c020b",
-  measurementId: "G-PHPCMW9FQK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
